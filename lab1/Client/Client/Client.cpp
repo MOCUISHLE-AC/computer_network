5708d044a514c1ba3907e75252c2a103eb382800
MOCUISHLE-AC <3061837128@qq.com>
2022-10-20 20:59:33 +0800

    finished lab1

---
+++ lab1/Client/Client/Client.cpp
@@ -0,0 +1,111 @@
+#pragma warning(disable:4996)
+#include <winsock2.h>//winsock2的头文件
+#include <iostream>
+#include <windows.h>
+#include <cstring>
+using  namespace std;
+
+//勿忘，链接dll的lib
+#pragma comment(lib, "ws2_32.lib")
+
+DWORD WINAPI recvMsgThread(LPVOID IpParameter);
+int  main()
+{
+
+	//加载winsock2的环境
+	WSADATA  wd;
+	if (WSAStartup(MAKEWORD(2, 2), &wd) != 0)
+	{
+		cout << "WSAStartup  error：" << GetLastError() << endl;
+		return 0;
+	}
+
+	//创建流式套接字
+	SOCKET  s = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	if (s == INVALID_SOCKET)
+	{
+		cout << "socket  error：" << GetLastError() << endl;
+		return 0;
+	}
+
+	//链接服务器
+	sockaddr_in   addr;
+	addr.sin_family = AF_INET;
+	addr.sin_port = htons(6666);
+	addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+	cout << "connecting..." << endl;
+	int len = sizeof(sockaddr_in);
+	if (connect(s, (SOCKADDR*)&addr, len) == SOCKET_ERROR)
+	{
+		cout << "connect  error：" << GetLastError() << endl;
+		return 0;
+	}
+	cout << "输入用户名：";
+	char name[20] = { 0 };
+	cin.getline(name, 20);
+	send(s, name, 20, 0);
+	//接收服务端的消息
+	char buf[100] = { 0 };
+	recv(s, buf, 100, 0);
+	cout << buf << endl;
+	// 创建线程，并且传入与client通讯的套接字
+	HANDLE hThread = CreateThread(NULL, 0, recvMsgThread, (LPVOID)s, 0, NULL);
+	//3随时给服务端发消息
+	int  ret = 0;
+	while (true)
+	{
+		//获取聊天内容
+		char chat[100] = { 0 };
+		cout << ">";
+		cin.getline(chat, 100);
+		if (strlen(chat) != 0)
+		{
+			char quit[15] = "sudo quit";
+			if (strcmp(chat, quit) == 0)
+			{
+				cout << "退出成功！" << endl;
+				break;
+			}
+			//获取系统时间
+			char time[100] = { 0 };
+			SYSTEMTIME sys;
+			GetLocalTime(&sys);
+			sprintf(time, "\n%4d/%02d/%02d %02d:%02d:%02d\n", sys.wYear, sys.wMonth, sys.wDay, sys.wHour, sys.wMinute, sys.wSecond);
+			//拼接
+			strcat(chat, time);
+			send(s, chat, 100, 0);
+		}
+		else {
+			cout << "输入不能为空！！！" << endl;
+		}
+	}
+	CloseHandle(hThread); // 关闭对线程的引用
+	//4.关闭监听套接字
+	closesocket(s);
+
+	//清理winsock2的环境
+	WSACleanup();
+	system("pause");
+	return 0;
+}
+DWORD WINAPI recvMsgThread(LPVOID IpParameter)//接收消息的线程
+{
+	SOCKET cliSock = (SOCKET)IpParameter;//获取客户端的SOCKET参数
+	int ret = 0;
+	while (1)
+	{
+		if (ret != SOCKET_ERROR)
+		{
+			char buf[100] = { 0 };
+			ret = recv(cliSock, buf, 100, 0);
+			cout << buf << endl;
+		}
+		else
+		{
+			break;
+		}
+	}
+	return 0;
+}
+
+
---
+++ lab1/Server/Server/Server.cpp
@@ -0,0 +1,167 @@
+#pragma warning(disable:4996)
+#include <winsock2.h> // winsock2的头文件
+#include <WS2tcpip.h>
+#include <iostream>
+#pragma comment(lib, "ws2_32.lib")
+#define max_num 10 //Client最大数量
+#define port 6666 //默认端口号
+using namespace std;
+
+int total=0;//Client的数量
+SOCKET cliSock[max_num] = {0};//客户端套接字 
+SOCKADDR_IN cliAddr[max_num] = {0};//客户端地址
+char cliname[max_num][20] = {0};
+
+// stdcall的线程处理函数
+DWORD WINAPI ThreadFun(LPVOID lpThreadParameter);
+DWORD WINAPI WacthThread(LPVOID lpThreadParameter) {
+	while (true) {
+		char order[20] = { 0 };//服务器的命令
+		cin.getline(order, 20);
+		if (strcmp(order, "sudo ls") == 0) {
+			if (total == 0) {
+				cout << "聊天室人数：" << total << endl << endl;
+			}
+			else {
+				cout << "聊天室人数：" << total << endl;
+				for (int i = 0; i < total; i++) {
+					cout << cliname[i] <<"socket:"<< cliSock[i]<<endl;
+				}
+				cout << endl;
+			}
+		}
+	}
+	return 0;
+}
+int main()
+{
+	WSADATA wd;
+	if (WSAStartup(MAKEWORD(2, 2), &wd) != 0)
+	{
+		cout << "WSAStartup Error:" << WSAGetLastError() << endl;
+		return 0;
+	}
+
+	// 1. 创建流式套接字
+	SOCKET server_socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	if (server_socket == INVALID_SOCKET)
+	{
+		cout << "socket error:" << WSAGetLastError() << endl;
+		return 0;
+	}
+
+	// 2. 绑定端口和ip
+	sockaddr_in addr;
+	memset(&addr, 0, sizeof(sockaddr_in));
+	addr.sin_family = AF_INET;
+	addr.sin_port = htons(port);
+	addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+	cout << "默认端口号：" << port << endl;
+	cout << "默认IP地址：" << "127.0.0.1" << endl;
+
+	int len = sizeof(sockaddr_in);
+	if (bind(server_socket, (SOCKADDR*)&addr, len) == SOCKET_ERROR)
+	{
+		cout << "bind Error:" << WSAGetLastError() << endl;
+		return 0;
+	}
+	cout << "server建立成功" << endl << endl;
+	// 3. 监听
+	listen(server_socket, max_num);
+	HANDLE Recv_thread = CreateThread(NULL, 0, WacthThread, (LPVOID)server_socket, 0, NULL);
+	// 主线程循环接收客户端的连接
+	while (true)
+	{
+		sockaddr_in addrClient;
+		len = sizeof(sockaddr_in);
+		// 4.接受成功返回与client通讯的Socket
+		SOCKET c = accept(server_socket, (SOCKADDR*)&addrClient, &len);
+		if (c != INVALID_SOCKET)
+		{
+			cliSock[total] = c;
+			cliAddr[total] = addrClient;
+			// 创建线程，并且传入与client通讯的套接字
+			HANDLE Recv_thread = CreateThread(NULL, 0, ThreadFun, (LPVOID)c, 0, NULL);
+			CloseHandle(Recv_thread); // 关闭对线程的引用
+		}
+	}
+
+	// 6.关闭监听套接字
+	closesocket(server_socket);
+
+	// 清理winsock2的环境
+	WSACleanup();
+
+	return 0;
+}
+
+DWORD WINAPI ThreadFun(LPVOID lpThreadParameter)
+{
+	// 5.与客户端通讯，发送或者接受数据
+	total++;
+	SOCKET c = (SOCKET)lpThreadParameter;
+	char temp[20];
+	recv(c, temp, 20, 0);
+	int index=-1;
+	for (int i = 0; i < total; i++) {
+		if (c == cliSock[i]) {
+			index = i;
+			break;
+		}
+	}
+	
+	cout << "欢迎" << temp << "进入聊天室！" <<endl;
+
+	strcpy(cliname[index], temp);
+	// 发送数据
+	char welcome[100] = { 0 };
+	sprintf(welcome, "欢迎 %s 进入聊天室！", cliname[index]);
+	send(c, welcome, 100, 0);
+
+	// 循环接收客户端数据
+	int ret = 0;
+	do
+	{
+		//接受Client发送的消息
+		char receive[100] = { 0 };
+		ret = recv(c, receive, 100, 0);
+		char spread[100] = { 0 };
+		//某些Client线程推出后，需要重新定位
+		int index = -1;
+		for (int i = 0; i < total; i++) {
+			if (c == cliSock[i]) {
+				index = i;
+				break;
+			}
+		}
+		sprintf(spread, "\n%s说：", cliname[index]);
+		strcat(spread, receive);
+		if (ret != 0 && ret != SOCKET_ERROR)
+		{
+			cout << spread;
+			for (int i = 0; i < total; i++) {
+				if (c != cliSock[i]) {
+					send(cliSock[i], spread, 100, 0);
+				}
+			}
+		}
+		else {
+			sprintf(spread, "\n%s退出了聊天!!!\n", cliname[index]);
+			cout << endl << cliname[index] << "离开了聊天室!!!" << endl;
+			for (int i = 0; i < total; i++) {
+				if (c != cliSock[i]) {
+					send(cliSock[i], spread, 100, 0);
+				}
+			}
+		}
+	} while (ret != SOCKET_ERROR && ret != 0);
+
+	//退出线程前，将Client数组向前移动
+	for (int i = index; i < total; i++) {
+		cliSock[i] = cliSock[i + 1];
+		cliAddr[i] = cliAddr[i + 1];
+		strcpy(cliname[i], cliname[i + 1]);
+	}
+	total--;
+	return 0;
+}
